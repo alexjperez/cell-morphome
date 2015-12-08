9ff45d7e63e78f9f9cb7b141d091338250087c8d
Alex Perez <alexjperez@gmail.com>
2015-12-07 20:49:37 -0800

    Changed manner in which the command is printed

--- rand_to_rgb.sh
+++ rand_to_rgb.sh
@@ -1,19 +1,26 @@
-#! /bin/bash
+#!/bin/bash
+#
+# Parses the current directory for model files with the 'rand' color ID in
+# their filename. Converts each file to the 'rgb' color ID format, with the
+# assigned names and colors corresponding to the desired values for the
+# given biological structure or organelle.
+#
+# Written by: Alex Perez - alexjperez@gmail.com on 2015-12-07
 
-# Define IMOD R,G,B lookup table
+# Define IMOD R,G,B values for each structure
 declare -A rgb
-rgb[plasmamembrane]=1,1,1
-rgb[primarycilium]=1,1,1
-rgb[nucleus]=1,1,0
-rgb[nucleolus]=0,0,1
-rgb[mitochondrion]=0,1,0
-rgb[lysosome]=1,0,0
-rgb[stigmoidbody]=0,1,1
-rgb[multivesicularbody]=0.2,0,0.8
-rgb[autophagosome]=1,0.5,1
-rgb[autolysosome]=1,0,1
+rgb[plasmamembrane]="1,1,1"
+rgb[primarycilium]="1,1,1"
+rgb[nucleus]="1,1,0"
+rgb[nucleolus]="0,0,1"
+rgb[mitochondrion]="0,1,0"
+rgb[lysosome]="1,0,0"
+rgb[stigmoidbody]="0,1,1"
+rgb[multivesicularbody]="0.2,0,0.8"
+rgb[autophagosome]="1,0.5,1"
+rgb[autolysosome]="1,0,1"
 
-# Define IMOD name lookup table
+# Define IMOD object names for each structure
 declare -A name
 name[plasmamembrane]="Plasma Membrane"
 name[primarycilium]="Primary Cilium"
@@ -26,17 +33,26 @@ name[multivesicularbody]="Multivesicular Body"
 name[autophagosome]="Autophagosome"
 name[autolysosome]="Autolysosome"
 
-# Run edmod for each file
+# Loop over all files with the 'rand' color identifier
 for file in *rand*.mod; do
-    base=$( echo $file | cut -d "_" -f1-5 )
-    structure=$( echo $file | cut -d "_" -f7 | cut -d "." -f1 )
-    file_name=${name[$structure]}
-    file_color=${rgb[$structure]}
-    if [ -z "$file_color" ]; then
-        echo "ERROR: No entry for structure named $structure"
+
+    # Extract base and structure strings from the file name
+    base="$(echo "${file}" | cut -d "_" -f1-5)"
+    structure="$(echo "${file}" | cut -d "_" -f7 | cut -d "." -f1)"
+
+    # Get the correct name and color values from the associative arrays
+    file_name="${name[$structure]}"
+    file_color="${rgb[$structure]}"
+
+    # If entries do not exist, print error and continue. If they do, run 
+    # edmod, changing the color, name, and line width. The 'col' identifier
+    # will be changed from rand to rgb.
+    if [[ -z "$file_color" ]]; then
+        printf "ERROR: No entry for structure named %s\n" "${structure}"
     else
-        echo -e "edmod.py --nameout \"$file_name\" --colorout $file_color --linewidth 3 $file ${base}_rgb_${structure}.mod"
-        edmod.py --nameout "$file_name" --colorout $file_color --linewidth 3 $file ${base}_rgb_${structure}.mod
+        set -x
+        edmod.py --nameout "${file_name}" --colorout "${file_color}" \
+            --linewidth 3 "${file}" "${base}"_rgb_"${structure}".mod
+        { set +x; } 2> /dev/null
     fi 
 done
-
